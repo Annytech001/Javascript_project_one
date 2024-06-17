d32602c828de9813ee44ad597a8cb7532c06efbd
Annytech001 <godlybassey101@gmail.com>
2024-06-17 16:15:21 +0100

    submit Assignment

---
+++ index.js
@@ -0,0 +1,60 @@
+/*
+let country = "Nigeria";
+let continent ="Africa";
+let population = 30000;
+
+console.log(country)
+console.log(continent)
+console.log(population)
+*/
+
+//function calcAge(ageInYears) {
+//	return ageInYears * 365;
+//}
+//console.log(calcAge(50));
+//console.log(calcAge(4));
+//console.log(calcAge(10));
+/*
+function convert(minutes) {
+	return minutes * 60;
+}
+
+console.log(convert(10)); 
+console.log(convert(15));
+console.log(convert(20)); 
+*/
+/*
+function triArea(base, height) {
+	return (base * height) / 2;
+}
+console.log(triArea(5, 3)); 
+console.log(triArea(10, 4));
+console.log(triArea(15, 6)); 
+*/
+/*
+function howManySeconds(hours) {
+    return hours * 60 * 60;
+}
+console.log(howManySeconds(5)); 
+console.log(howManySeconds(15));
+console.log(howManySeconds(20));
+*/
+
+// function circuitPower(voltage, current) {
+// 	return voltage * current;
+// }
+// console.log(circuitPower(220, 10));
+// console.log(circuitPower(120, 30)); 
+// console.log(circuitPower(280, 50)); 
+
+// function getFirstValue(arr) {
+//     return 
+// }
+
+function lessThanOrEqualToZero(num) {
+    return num <= 0;
+}
+console.log(lessThanOrEqualToZero(9)); 
+console.log(lessThanOrEqualToZero(0)); 
+console.log(lessThanOrEqualToZero(-4));
+
